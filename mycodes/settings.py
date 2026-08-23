d67da4c5ce154e69220aeaf202f89216fedaa1c2
parham shafaghati <156317336+psh8714@users.noreply.github.com>
2026-08-23 09:52:55 +0330

    Update settings.py

--- mycodes/settings.py
+++ mycodes/settings.py
@@ -125,7 +125,7 @@ WSGI_APPLICATION = 'mycodes.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database' /'db.sqlite3',
     }
 }
 
